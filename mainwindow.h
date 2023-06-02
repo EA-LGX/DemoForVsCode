0913bae87a34cb2d5c96719dfce97dae6fa47333
陆广兴 <1175278814@qq.com>
2023-06-02 17:35:26 +0800

    更新 README.md

--- mainwindow.h
+++ mainwindow.h
@@ -7,15 +7,14 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
   Q_OBJECT
 
- public:
-  MainWindow(QWidget *parent = nullptr);
+public:
+  MainWindow(QWidget* parent = nullptr);
   ~MainWindow();
 
- private:
-  Ui::MainWindow *ui;
+private:
+  Ui::MainWindow* ui;
 };
 #endif // MAINWINDOW_H
